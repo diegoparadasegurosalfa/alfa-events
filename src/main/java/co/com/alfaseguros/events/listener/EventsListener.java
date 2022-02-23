b858c9cd73a06a7466058d10a1f7b4628c0c1286
DESKTOP-DA1AFGP\Rafael <rafael.pereira@segurosalfa.com.co>
2022-02-23 08:09:40 -0500

    Casteo de datos

--- src/main/java/co/com/alfaseguros/events/listener/EventsListener.java
+++ src/main/java/co/com/alfaseguros/events/listener/EventsListener.java
@@ -16,6 +16,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 @Component
 @Log4j2
@@ -42,10 +45,32 @@ public class EventsListener {
 	
 	@SqsListener(value = "${alfa.queues.auditinpaymentregistry.name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
 	public void setRecordEvent(String message) throws ExceptionAlfa {
-		log.debug("Received Message {}", message);
+		//log.debug("Received Message {}", message);
         ObjectMapper objectMapper = new ObjectMapper();
         try {
         	SetRecordEventRequest setRecordEventRequest = objectMapper.readValue(message, SetRecordEventRequest.class);
+
+			String validityFee = setRecordEventRequest.getParametersList().get(7).getValue();
+			String validityFeeCast = validityFee.replaceAll("\\.([0-9]+)", "").replaceAll("\\,", "");
+			setRecordEventRequest.getParametersList().get(7).setValue(validityFeeCast);
+
+			String strStartDt = setRecordEventRequest.getParametersList().get(2).getValue();
+			String strScheduleDt = setRecordEventRequest.getParametersList().get(6).getValue();
+
+
+			LocalDate dateStartDt = LocalDate.parse(strStartDt);
+			LocalDate dateScheduleDt = LocalDate.parse(strScheduleDt);
+
+			LocalDateTime dateTimeStartDt = dateStartDt.atStartOfDay();
+			LocalDateTime dateTimeScheduleDt = dateScheduleDt.atStartOfDay();
+
+			setRecordEventRequest.getParametersList().get(2).setValue(dateTimeStartDt.toString());
+			setRecordEventRequest.getParametersList().get(6).setValue(dateTimeScheduleDt.toString());
+
+
+
+			log.debug("Received Message {}", setRecordEventRequest);
+
         	setRecordEventServiceExecution.processOperation(setRecordEventRequest);
 		} catch (JsonProcessingException exception) {
 			throw new ClientExceptionAlfa(MessageResponseEnum.DATA_VALIDATION, exception.getMessage());
